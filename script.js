345c35d7367a69e6c679cb427a6512631c8d5444
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-11 16:04:58 +0200

    edit the html and css fils and finally the multiplier works

--- script.js
+++ script.js
@@ -1,40 +1,49 @@
 // DOM objects
-const button = document.getElementById('run');
 let count = 0;
+const button = document.getElementById('run');
 const counter = document.getElementById('count');
 const button1 = document.getElementById('multi-btn');
 let multiplier = 2;
-let multipCost = 20;
+let increaseByOne = 1;
+let multipCost = 10;
 const multiplierCost = document.getElementById('multi-cost');
 const autoClickBtn = document.getElementById('auto-click');
-let autoClick = 1;
-
+const message = document.getElementById('msg');
+const changNum = document.getElementById('changeNum');
+let num = 2;
 
 
 
 // click button when you click the cookie the score will increase by 1
 button.addEventListener("click", () => {
-        count += 1;
+        count += increaseByOne;
         counter.innerHTML = count;
 
 });
 
 
-// second button that make the score increase by 2
+// second button that make the score increase the value of the score
 button1.addEventListener('click',  () => {
 
     if (count >= multipCost) {
+        increaseByOne += 1;
         count = count - multipCost;
-        count = count * 2;
         multipCost = multipCost * multiplier;
-        multiplierCost.innerHTML = multipCost;
+       multiplierCost.innerHTML = multipCost;
         counter.innerHTML = count;
+        changNum.innerHTML = ++num;
+    } else if (count !== multipCost) {
+        message.innerHTML = "You don\'t have enough points";
 
     }
 
 });
 // auto clicker
 autoClickBtn.addEventListener('click', () => {
+
+
+
+
 
 });
 
