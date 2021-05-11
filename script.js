4417082b6487b71e7897e6cb2662bace3b520c1e
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-11 11:07:58 +0200

    some modifications to the html & style files, and still trying with js file

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 // DOM objects
 const button = document.getElementById('run');
-const counter = document.getElementById('count');
 let count = 0;
-const button1 = document.getElementById('run1');
+const counter = document.getElementById('count');
+const button1 = document.getElementById('multi-btn');
+let multiplier = 2;
+let multipCost = 20;
+const multiplierCost = document.getElementById('multi-cost');
+const autoClickBtn = document.getElementById('auto-click');
+let autoClick = 1;
+
+
 
 
 // click button when you click the cookie the score will increase by 1
 button.addEventListener("click", () => {
-
-        counter.innerHTML = ++count;
+        count += 1;
+        counter.innerHTML = count;
 
 });
 
 
 // second button that make the score increase by 2
-button1.addEventListener('click', () => {
+button1.addEventListener('click',  () => {
 
-})
+    if (count >= multipCost) {
+        count = count - multipCost;
+        count = count * 2;
+        multipCost = multipCost * multiplier;
+        multiplierCost.innerHTML = multipCost;
+        counter.innerHTML = count;
+
+    }
+
+});
+// auto clicker
+autoClickBtn.addEventListener('click', () => {
+
+});
+
